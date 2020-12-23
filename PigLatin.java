59353e71f8bf2f199415237f0038a113673fb2c9
J3rry-L <jliang21@stuy.edu>
2020-12-23 15:36:11 -0500

    Added safety check for strings of length 1

--- PigLatin.java
+++ PigLatin.java
@@ -19,8 +19,11 @@ public class PigLatin{
     if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
       return(s + "hay");
     }
-    else{
+    else if (s.length() > 1){
       return(s.substring(1) + s.charAt(0) + "ay");
+    }
+    else{
+      return(s.charAt(0) + "ay");
     }
   }
   public static String pigLatin(String s){
