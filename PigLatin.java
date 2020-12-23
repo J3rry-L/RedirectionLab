9c1b8a03dcec87e81d9b966d7762b48891233315
J3rry-L <jliang21@stuy.edu>
2020-12-23 15:27:44 -0500

    Fixed small hole involving strings of length 0

--- PigLatin.java
+++ PigLatin.java
@@ -1,12 +1,20 @@
 import java.util.*;
 public class PigLatin{
   public static void main(String[] args){
-    System.out.println(pigLatinSimple("mock"));
-    System.out.println(pigLatinSimple("pie"));
-    System.out.println(pigLatinSimple("david"));
-    System.out.println(pigLatinSimple("aaron"));
+    System.out.println(pigLatinSimple("mOck"));
+    System.out.println(pigLatinSimple("pIe"));
+    System.out.println(pigLatinSimple("daViD"));
+    System.out.println(pigLatinSimple("AaRon"));
+    System.out.println(pigLatinSimple("a"));
+    System.out.println(pigLatinSimple("A"));
+    System.out.println(pigLatinSimple("z"));
+    System.out.println(pigLatinSimple("Z"));
+    System.out.println(pigLatinSimple(""));
   }
   public static String pigLatinSimple(String s){
+    if (s == ""){
+      return "";
+    }
     s = s.toLowerCase();
     if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
       return(s + "hay");
