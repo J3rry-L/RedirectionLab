f07d210c2641b94c911dd1add22eff8d3fd55684
J3rry-L <jliang21@stuy.edu>
2020-12-23 15:55:22 -0500

    Created pigLatin method and tested

--- PigLatin.java
+++ PigLatin.java
@@ -10,6 +10,21 @@ public class PigLatin{
     System.out.println(pigLatinSimple("z"));
     System.out.println(pigLatinSimple("Z"));
     System.out.println(pigLatinSimple(""));
+    System.out.println(pigLatin("tHe"));
+    System.out.println(pigLatin("CheCk"));
+    System.out.println(pigLatin("skeE"));
+    System.out.println(pigLatin("eMU"));
+    System.out.println(pigLatin("gRade"));
+    System.out.println(pigLatin("tH"));
+    System.out.println(pigLatin("mOck"));
+    System.out.println(pigLatin("pIe"));
+    System.out.println(pigLatin("daViD"));
+    System.out.println(pigLatin("AaRon"));
+    System.out.println(pigLatin("a"));
+    System.out.println(pigLatin("A"));
+    System.out.println(pigLatin("z"));
+    System.out.println(pigLatin("Z"));
+    System.out.println(pigLatin(""));
   }
   public static String pigLatinSimple(String s){
     if (s == ""){
@@ -27,7 +42,19 @@ public class PigLatin{
     }
   }
   public static String pigLatin(String s){
-    return null;
+    ArrayList<String> digraphs = new ArrayList<String>(Arrays.asList("bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"));
+    s = s.toLowerCase();
+    if (s.length() > 1 && digraphs.contains(s.substring(0,2))){
+      if(s.length() == 2){
+        return(s + "ay");
+      }
+      else{
+        return(s.substring(2) + s.substring(0, 2) + "ay");
+      }
+    }
+    else{
+      return(pigLatinSimple(s));
+    }
   }
   public static String pigLatinBest(String s){
     return null;
