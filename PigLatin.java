ce48d3c272393155c3db4f9bdf330473ad5b2889
J3rry-L <jliang21@stuy.edu>
2020-12-23 15:23:16 -0500

    Created pigLatinSimple method and did prelim testing

--- PigLatin.java
+++ PigLatin.java
@@ -1,15 +1,24 @@
-import java.util.*
+import java.util.*;
 public class PigLatin{
   public static void main(String[] args){
-
+    System.out.println(pigLatinSimple("mock"));
+    System.out.println(pigLatinSimple("pie"));
+    System.out.println(pigLatinSimple("david"));
+    System.out.println(pigLatinSimple("aaron"));
   }
   public static String pigLatinSimple(String s){
-
+    s = s.toLowerCase();
+    if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
+      return(s + "hay");
+    }
+    else{
+      return(s.substring(1) + s.charAt(0) + "ay");
+    }
   }
   public static String pigLatin(String s){
-
+    return null;
   }
   public static String pigLatinBest(String s){
-
+    return null;
   }
 }
