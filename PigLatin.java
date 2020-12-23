33c58a15a67e25f1c48518878545b4773b54dbfa
J3rry-L <jliang21@stuy.edu>
2020-12-23 13:47:44 -0500

    Initialized three methods

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,15 @@
 import java.util.*
 public class PigLatin{
   public static void main(String[] args){
-    
+
+  }
+  public static String pigLatinSimple(String s){
+
+  }
+  public static String pigLatin(String s){
+
+  }
+  public static String pigLatinBest(String s){
+
   }
 }
